7cdbc3f4b74e13285108162559a1d7617c5c42a0
kiyoshigawa <tim@twa.ninja>
2026-04-25 12:15:39 -0600

    Initial Commit - Template Deploy and License Updates

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+//! IIDX_Controller_v2
+//! Author: kiyoshigawa
+//! Date Started: 2026-04-25
+//!
+//! This is a project for a beatmania IIDX controller controller board.
+//! This project contains code under multiple licenses:
+//!
+//! - Original template code from rp-rs/rp235x-project-template:
+//!   Dual-licensed under MIT OR Apache-2.0
+//!
+//! - New code and modifications by [Your Name]:
+//!   Licensed under GPLv3
+//!
+//! SPDX-License-Identifier: GPL-3.0-or-later
+
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use defmt_rtt as _;
+use embedded_hal::digital::OutputPin;
+use panic_probe as _;
+use rp235x_hal::clocks::init_clocks_and_plls;
+use rp235x_hal::{self as hal, entry};
+use rp235x_hal::{Clock, pac};
+
+/// Tell the Boot ROM about our application:
+#[unsafe(link_section = ".start_block")]
+#[used]
+pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
+
+#[entry]
+fn main() -> ! {
+    // system peripheral setup:
+    info!("Program start");
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = cortex_m::Peripherals::take().unwrap();
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let sio = hal::Sio::new(pac.SIO);
+
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    loop {}
+}
+
+/// Program metadata for `picotool info`
+#[unsafe(link_section = ".bi_entries")]
+#[used]
+pub static PICOTOOL_ENTRIES: [rp235x_hal::binary_info::EntryAddr; 5] = [
+    rp235x_hal::binary_info::rp_cargo_bin_name!(),
+    rp235x_hal::binary_info::rp_cargo_version!(),
+    rp235x_hal::binary_info::rp_program_description!(c"RP2350 Template"),
+    rp235x_hal::binary_info::rp_cargo_homepage_url!(),
+    rp235x_hal::binary_info::rp_program_build_attribute!(),
+];
+
+// End of file
